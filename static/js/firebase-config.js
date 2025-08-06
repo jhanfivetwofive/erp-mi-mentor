157f7d64006ea8f3d87564811734d5ddda651972
Jonathan Vázquez Pineda <jonathan@fivetwofive.mx>
2025-08-06 14:14:23 -0600

    Fix: uso correcto de auth.verify_id_token para Firebase login

---
+++ static/js/firebase-config.js
@@ -0,0 +1,7 @@
+// static/js/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCVnGrLvUYx_BnhUn-MmNk9N97WA84r7QI",
+  authDomain: "mimentorerp.firebaseapp.com",
+  projectId: "mimentorerp",
+};
+firebase.initializeApp(firebaseConfig);
